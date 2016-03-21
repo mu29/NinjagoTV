d02b590484de1489febad00d8abe69fec2183b0b
InJung Chung <mu29gl@gmail.com>
2016-03-21 16:09:52 +0900

    어댑터 아이템 빌더 클래스

---
+++ app/src/main/java/net/pedaling/ninjagotv/view/builder/Builder.java
@@ -0,0 +1,26 @@
+package net.pedaling.ninjagotv.view.builder;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by InJung on 2016. 3. 21..
+ */
+public abstract class Builder {
+
+    protected View mView;
+    protected ViewGroup mParent;
+    protected Context mContext;
+
+    public Builder(Context context, View convertView, ViewGroup parent) {
+        mContext = context;
+        mView = convertView;
+        mParent = parent;
+    }
+
+    public abstract int getViewType();
+
+    public abstract View buildView();
+
+}
