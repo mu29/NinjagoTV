8e126bac8b658ccc800b4366e2bd9fa10c25d67c
InJung Chung <mu29gl@gmail.com>
2016-03-21 16:10:16 +0900

    메인 프레젠터 뼈대

---
+++ app/src/main/java/net/pedaling/ninjagotv/presenter/MainPresenter.java
@@ -0,0 +1,15 @@
+package net.pedaling.ninjagotv.presenter;
+
+import net.pedaling.ninjagotv.MvpPresenter;
+import net.pedaling.ninjagotv.MvpView;
+
+/**
+ * Created by InJung on 2016. 3. 21..
+ */
+public class MainPresenter extends MvpPresenter<MainPresenter.MainView> {
+
+    public interface MainView extends MvpView {
+
+    }
+
+}
